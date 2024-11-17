891bfd0fb5582ab07bc6f8fbf6e34da568f4f5ac
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-11-17 19:44:03 -0300

    settings page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:ricka_and_morty/home/page/home_page.dart';
 import 'package:ricka_and_morty/injection.dart';
@@ -5,7 +6,13 @@ import 'package:ricka_and_morty/injection.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   configureDependencies();
-  runApp(const RickAndMortyApp());
+  runApp(
+    DevicePreview(
+      builder: (_) {
+        return const RickAndMortyApp();
+      },
+    ),
+  );
 }
 
 class RickAndMortyApp extends StatelessWidget {
---
+++ lib/settings/presentation/page/settings_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:ricka_and_morty/settings/presentation/widgets/settings_widget.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SettingsWidget();
+  }
+}
